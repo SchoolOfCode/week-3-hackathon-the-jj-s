e6358f3f4b683a252aba8f243015e19b31bd8564
sumeyacodes <sumeya4h@gmail.com>
2024-12-12 16:13:13 +0000

    added specific subreddit to get memes from, next refractor will need a function for display meme

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ const generateMemeBtn = document.getElementById("meme-button")
 //we need an async await function for fetching the API
 async function fetchMemeApi() {
     try {
-        const url = "https://meme-api.com/gimme";
+        const subreddit = 'ProgrammerHumor'
+        const url = `https://meme-api.com/gimme/${subreddit}`;
         const response = await fetch(url);
         const data = await response.json();
         console.log(data);
         
+        // refractor this into seperate function
             memeTitle.innerText = data.title;
             memeImage.src = data.url;
             memeImage.alt = data.title;
@@ -34,4 +36,4 @@ generateMemeBtn.addEventListener("click", fetchMemeApi);
 
 //Things to fix
     // nsfw memes still appearing randomly
-    // add loading page or any loading
+    // 
